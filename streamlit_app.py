dd401ba531a6843dd4b34c519e1aefcb926c6217
alisharayna <alisharaynaazzahra138@gmail.com>
2025-05-22 08:45:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG-20250512-WA0102.jpg", width=200)
